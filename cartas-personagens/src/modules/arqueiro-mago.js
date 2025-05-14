1fcef8eb61e13f09a31616a92a1b496222c980e8
Ana Daré <analauradare467@gmail.com>
2025-05-13 21:39:40 -0300

    Adicionando static as classes

--- cartas-personagens/src/modules/arqueiro-mago.js
+++ cartas-personagens/src/modules/arqueiro-mago.js
@@ -5,7 +5,8 @@ import { Mago } from "./mago.js";
 export class ArqueiroMago extends Personagem {
     ladoArqueiro
     ladoMago
-    tipo = 'ArqueiroMago'
+    static tipo = 'ArqueiroMago'
+    static descricao ='Detentor de lancas e flechas mágicas!'
 
     constructor(nome, level, destreza, elementoMagico, levelMagico, inteligencia) {
         super(nome, level)
